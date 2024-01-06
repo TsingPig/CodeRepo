313edfc52e5926a00326a09fbf17d184386f294d
tsingpig <1114196607@qq.com>
2024-01-06 23:40:33 +0800

    vscode配置

--- DesignPattern/c_plus_plus/Decorator/Decorator.cpp
+++ DesignPattern/c_plus_plus/Decorator/Decorator.cpp
@@ -1,67 +1,79 @@
 #include <iostream>
 
 // 抽象组件
-class Player {
+class Player
+{
 public:
     virtual void Equip() = 0;
     virtual ~Player() {}
 };
 
 // 具体组件
-class BasicPlayer : public Player {
+class BasicPlayer : public Player
+{
 public:
-    void Equip() override {
+    void Equip() override
+    {
         std::cout << "Basic equipment equipped" << std::endl;
     }
 };
 
 // 抽象装饰类
-class PlayerDecorator : public Player {
+class PlayerDecorator : public Player
+{
 protected:
-    Player* player;
+    Player *player;
 
 public:
-    PlayerDecorator(Player* p) : player(p) {}
+    PlayerDecorator(Player *p) : player(p) {}
 
-    void Equip() override {
-        if (player != nullptr) {
+    void Equip() override
+    {
+        if (player != nullptr)
+        {
             player->Equip();
         }
     }
 
-    virtual ~PlayerDecorator() {
+    virtual ~PlayerDecorator()
+    {
         delete player;
     }
 };
 
 // 具体装饰类
-class SwordDecorator : public PlayerDecorator {
+class SwordDecorator : public PlayerDecorator
+{
 public:
-    SwordDecorator(Player* p) : PlayerDecorator(p) {}
+    SwordDecorator(Player *p) : PlayerDecorator(p) {}
 
-    void Equip() override {
+    void Equip() override
+    {
         PlayerDecorator::Equip();
         std::cout << "Sword equipped" << std::endl;
     }
 };
 
-class ShieldDecorator : public PlayerDecorator {
+class ShieldDecorator : public PlayerDecorator
+{
 public:
-    ShieldDecorator(Player* p) : PlayerDecorator(p) {}
+    ShieldDecorator(Player *p) : PlayerDecorator(p) {}
 
-    void Equip() override {
+    void Equip() override
+    {
         PlayerDecorator::Equip();
         std::cout << "Shield equipped" << std::endl;
     }
 };
 
 // 使用
-int main() {
+int main()
+{
     // 创建基本玩家
-    Player* basicPlayer = new BasicPlayer();
+    Player *basicPlayer = new BasicPlayer();
 
     // 添加剑和盾的装备
-    Player* decoratedPlayer = new ShieldDecorator(new SwordDecorator(basicPlayer));
+    Player *decoratedPlayer = new ShieldDecorator(new SwordDecorator(basicPlayer));
 
     // 游戏中动态装备
     decoratedPlayer->Equip();
