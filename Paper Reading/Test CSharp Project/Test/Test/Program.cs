cab5093cb95bf1a35a3a20100a823ab0084c099d
tsingpig <1114196607@qq.com>
2025-01-19 21:30:16 +0800

    Pre ppt 2025.1.21

---
+++ Paper Reading/Test CSharp Project/Test/Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 5, 2, 9, 1 };
+
+        Array.Sort(numbers, new IntComparer());
+        Console.WriteLine(string.Join(", ", numbers)); // 1, 2, 5, 9
+    }
+
+    // 自定义比较器实现 IComparer<int>
+    class IntComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+}
